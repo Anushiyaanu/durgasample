faa605ffcf9eac1dd1b73a6f18ce55fb06b4c212
Anushiya <102012622+Anushiyaanu@users.noreply.github.com>
2023-07-24 05:40:59 +0000

    inserted

---
+++ active/Nes.java
@@ -0,0 +1,18 @@
+import java.util.Collection;  
+import java.util.Iterator;  
+import java.util.concurrent.ConcurrentLinkedQueue;  
+public class Nes{  
+    static int i = 1;  
+    public static void main(String[] args) {  
+        Collection<String> collection = new ConcurrentLinkedQueue<String>();  
+        collection.add("Ram");  
+        collection.add("Sham");  
+        collection.add("Mira");  
+        collection.add("Rajesh");  
+        Iterator<String> iterator = collection.iterator();  
+        //Returns an iterator over the elements  
+        while (iterator.hasNext()) {  
+            System.out.println(i++ + "." + iterator.next());  
+        }  
+    }  
+} 
